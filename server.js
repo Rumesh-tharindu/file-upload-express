a572abc8df5590263c00e218490b9063b8ecd7a2
rumesh-tharindu <rumeshtharindu2020@gmail.com>
2020-11-11 21:29:19 +0530

    chack responses

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app =express()
 
 app.get('/',(req,res)=>{
   
-    res.send("hi there ")
+    res.send("hi there you are awesome man ")
 })
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
