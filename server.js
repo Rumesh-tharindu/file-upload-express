8cd774474a7017eb3dfebf1df45be07f9df3616f
rumesh-tharindu <rumeshtharindu2020@gmail.com>
2020-11-11 22:38:55 +0530

    connect mysql database

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express= require('express')
 const bodyParser = require('body-parser')
 const app =express()
+const mysql=require('mysql')
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'my$077nethmi',
+    database: 'file-upload'
+  })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+connection.connect()
 app.get('/',(req,res)=>{
-   
-    res.json(
-        {username:req.body.username,
-        password:req.body.password}
-        )
+    let stmt = `INSERT INTO user(username,password)
+            VALUES(?,?)`
+    connection.query(stmt,[req.body.username,req.body.password],(err,result)=>{
+        if(err){
+            res.json(err)
+        }
+        else{
+            res.json(result)
+        }
+    })
+
 })
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
