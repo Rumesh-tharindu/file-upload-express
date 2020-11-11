58a20a3959249b8b20138b6057822397fec4c8a1
rumesh-tharindu <rumeshtharindu2020@gmail.com>
2020-11-11 21:57:02 +0530

    add routes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express= require('express')
-
+const bodyParser = require('body-parser')
 const app =express()
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/',(req,res)=>{
-  
-    res.send("hi there you are awesome man ")
+   
+    res.json(
+        {username:req.body.username,
+        password:req.body.password}
+        )
 })
 const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
